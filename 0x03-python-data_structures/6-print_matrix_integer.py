7a1060be050fe8d413ec30f05358bf10c0ef0912
GideonAmhaG <gideonamaha@gmail.com>
2023-01-09 07:47:34 +0000

    Fixed space at the end of each line

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,7 +2,12 @@
 def print_matrix_integer(matrix=[[]]):
     if matrix:
         for items in matrix:
+            i = 1;
+            length = len(items)
             for item in items:
-                print("{:d}".format(item), end=" ")
-            
+                if i == length:
+                    print("{:d}".format(item), end="")
+                else:    
+                    print("{:d}".format(item), end=" ")
+                i += 1
             print()
