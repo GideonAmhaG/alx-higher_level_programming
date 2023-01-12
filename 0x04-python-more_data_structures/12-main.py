5f078aab8ae59386e96920720ebe1f68ef1fd8d3
GideonAmhaG <gideonamaha@gmail.com>
2023-01-12 11:05:57 +0000

    Roman to numeral

--- 0x04-python-more_data_structures/12-main.py
+++ 0x04-python-more_data_structures/12-main.py
@@ -17,3 +17,9 @@ print("{} = {}".format(roman_number, roman_to_int(roman_number)))
 
 roman_number = "DCCVII"
 print("{} = {}".format(roman_number, roman_to_int(roman_number)))
+
+roman_number = "XCIX"
+print("{} = {}".format(roman_number, roman_to_int(roman_number)))
+
+roman_number = "CMXCIX"
+print("{} = {}".format(roman_number, roman_to_int(roman_number)))
--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,3 +1,17 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
+    if type(roman_string) is not str or roman_string is None:
+        return 0
+    
+    roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    _sum = 0
+    last = 0
 
+    for letter in roman_string:
+        if last == 0 or last >= roman[letter]:
+            _sum += roman[letter]
+        elif last < roman[letter]:
+            _sum += roman[letter] - (last * 2)
+        last = roman[letter]
+
+    return _sum
