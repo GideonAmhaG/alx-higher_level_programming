da8fa321e1998ba71f31f2e41a3a33364f589e99
GideonAmhaG <gideonamaha@gmail.com>
2023-01-24 19:14:12 +0000

    Square of hash

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+""" Module """
+
+
+class Square:
+    """ Square class """
+    def __init__(self, size=0):
+        """ Initilize function """
+        self.size = size
+
+    @property
+    def size(self):
+        """ Getter function """
+        return self.__size
+
+    @size.setter
+    def size(self, size):
+        """ Setter function """
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """ Returns are of the square """
+        return self.__size * self.__size
+
+    def my_print(self):
+        """ Prints a square of # """
+        if self.__size == 0:
+            print()
+        for i in range(1, self.area() + 1):
+            print("#", end="")
+            if i % self.__size == 0 and i > 0:
+                print()
