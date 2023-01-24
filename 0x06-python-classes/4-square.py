68f935ca7860101ce77be9d6f9df788091fc68e7
GideonAmhaG <gideonamaha@gmail.com>
2023-01-24 19:01:32 +0000

    Getter setter

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+""" Module """
+
+
+class Square:
+    """ Square class """
+    def __init__(self, size=0):
+        """ Initilize function """
+        self.size = size
+
+    @property
+    def size(self):
+        """ Getter function """
+        return self.__size
+
+    @size.setter
+    def size(self, size):
+        """ Setter function """
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """ Returns are of the square """
+        return self.__size * self.__size
