2d4bcf8631bb428ab734fef181f2b4e05981743d
GideonAmhaG <gideonamaha@gmail.com>
2023-01-30 18:50:16 +0000

    Set get

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -32,7 +32,7 @@ class Rectangle:
     def height(self, value):
         """ Setter function """
         if type(value) is not int:
-            raise TypeError("width must be an integer")
-        if width < 0:
-            raise ValueError("width must be >= 0")
+            raise TypeError("height must be an integer")
+        if height < 0:
+            raise ValueError("height must be >= 0")
         self._height = value
