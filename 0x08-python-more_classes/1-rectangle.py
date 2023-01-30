bde737d942b79e2425bea56c2bb7f0b18418d47d
GideonAmhaG <gideonamaha@gmail.com>
2023-01-30 18:48:21 +0000

    Set get

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+""" Rectangle module """
+
+
+class Rectangle:
+    """ Rectangle class """
+    def __init__(self, width=0, height=0):
+        """ Initilizing function """
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """ Getter function """
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """ Setter function """
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if width < 0:
+            raise ValueError("width must be >= 0")
+        self._width = value
+
+    @property
+    def height(self):
+        """ Getter function """
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """ Setter function """
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if width < 0:
+            raise ValueError("width must be >= 0")
+        self._height = value
