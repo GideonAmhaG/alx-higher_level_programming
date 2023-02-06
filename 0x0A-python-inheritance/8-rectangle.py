967742cdf5271cee950fc33d3883c9b78a97ed92
GideonAmhaG <gideonamha@gmail.com>
2023-02-06 23:23:06 +0300

    1st commit

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" Rectangle class module """
+
+
+BaseGeometry = __import__("7-base_geometry.").BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """ Rectangle class """
+    def __init__(self, width, height):
+        """ Initilizing class """
+        super().integer_validator("width", width)
+        super().integer_validator("height", height)
+        self.__width = width
+        self.__height = height
