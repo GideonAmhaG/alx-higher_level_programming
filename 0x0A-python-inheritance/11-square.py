59bea6c483006377c2d16c44bd7c181c3749243b
GideonAmhaG <gideonamha@gmail.com>
2023-02-07 00:00:17 +0300

    1st commit

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" Square class module """
+
+
+Rectangle = __import__("9-rectangle").Rectangle
+
+
+class Square(Rectangle):
+    """ Square class """
+    def __init__(self, size):
+        """ Initilizing method """
+        super().integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
+
+    def area(self):
+        """ Returns area """
+        return self.__size**2
+
+    def __str__(self):
+        """ Allows print to be called on the Square class """
+        return "[Square] {}/{}".format(self.__size, self.__size)
