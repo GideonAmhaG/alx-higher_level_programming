e7bd8f4eceb9131de810e3f512af44fb0711f723
GideonAmhaG <gideonamha@gmail.com>
2023-02-07 00:08:59 +0300

    1st commit

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" MyInt class module """
+
+
+class MyInt(int):
+    """ MyInt class """
+    def __eq__(self, other):
+        """ Inverts equal """
+        return super(MyInt, self).__ne__(other)
+
+    def __ne__(self, other):
+        """ Inverts not equal """
+        return super(MyInt, slef).__eq__(other)
