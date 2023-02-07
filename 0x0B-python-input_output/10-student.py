8c2d8aabb48f8abba75001cc1b9929b0a386259e
GideonAmhaG <gideonamha@gmail.com>
2023-02-07 14:16:46 +0300

    1st commit

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" Student class module """
+
+
+class Student:
+    """ Student class """
+    def __init__(self, first_name, last_name, age):
+        """ Intilizing function """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """ Retrieves a dictionary representation of a Student instance """
+        if attrs is not None:
+            my_dict = {}
+            for i in attrs:
+                if i in self.__dict__:
+                    my_dict[i] = self.__dict__[i]
+            return my_dict
+        return self.__dict__
