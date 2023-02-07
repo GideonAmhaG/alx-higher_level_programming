bff11ff78733d04f102a020eda096b63e189d30a
GideonAmhaG <gideonamha@gmail.com>
2023-02-07 12:58:27 +0300

    typo

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -6,4 +6,4 @@ from json import loads
 def from_json_string(my_str):
     """" Returns an object (Python data structure) represented by a JSON
     string """
-    return loads(my_obj)
+    return loads(my_str)
