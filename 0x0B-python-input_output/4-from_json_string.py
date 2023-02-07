4e063d809647f14ab1d285037f9a503e8608bd19
GideonAmhaG <gideonamha@gmail.com>
2023-02-07 12:57:18 +0300

    1st commit

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" From json string module """
+from json import loads
+
+
+def from_json_string(my_str):
+    """" Returns an object (Python data structure) represented by a JSON
+    string """
+    return loads(my_obj)
