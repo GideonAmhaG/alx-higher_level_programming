8b7c8331a48c38b404a72a82fee2bc480d8d5bf1
GideonAmhaG <gideonamha@gmail.com>
2023-02-07 15:32:29 +0300

    added empty string

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -5,6 +5,7 @@
 def append_after(filename="", search_string="", new_string=""):
     """" Inserts a line of text to a file, after each line containing a
     specific string """
+    text = ""
     with open(filename, encoding="utf-8") as f:
         for line in f:
             text += line
