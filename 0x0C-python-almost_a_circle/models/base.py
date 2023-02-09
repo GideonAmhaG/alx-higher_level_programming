a9d3d43f070ccdddf0a348129ec0162421a95a85
GideonAmhaG <gideonamha@gmail.com>
2023-02-09 11:07:48 +0300

    Base pushed

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" Base class module """
+
+
+class Base:
+    """ Manages id attribute in all future classes and avoids duplicating
+    the same code (by extension, same bugs) """
+    __nb_objects = 0
+    def __init__(self, id=None):
+        """ Class constructor """
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
