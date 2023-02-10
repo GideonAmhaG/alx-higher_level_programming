f2947d95f43295975c2557a28c24213aff56a4ff
GideonAmhaG <gideonamha@gmail.com>
2023-02-10 12:32:13 +0300

    fixed x and y value errors

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -50,8 +50,8 @@ class Rectangle(Base):
         """ Setter function """
         if type(value) is not int:
             raise TypeError("x must be an integer")
-        if value <= 0:
-            raise ValueError("x must be > 0")
+        if value < 0:
+            raise ValueError("x must be >= 0")
         self.__x = value
 
     @property
@@ -64,6 +64,6 @@ class Rectangle(Base):
         """ Setter function """
         if type(value) is not int:
             raise TypeError("y must be an integer")
-        if value <= 0:
-            raise ValueError("y must be > 0")
+        if value < 0:
+            raise ValueError("y must be >= 0")
         self.__y = value
