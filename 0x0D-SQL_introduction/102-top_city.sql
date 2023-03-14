780723f5b6f1464a024630df4ea5d819d358ab56
GideonAmhaG <gideonamha@gmail.com>
2023-03-14 17:41:48 +0300

    changed is with =

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -2,7 +2,7 @@
 -- by temperature (descending)
 SELECT city, AVG(value) AS avg_temp
 FROM temperatures
-WHERE month IS 7 OR month IS 8
+WHERE month = 7 OR month = 8
 GROUP BY city
 ORDER BY avg_temp DESC
 LIMIT 3;
