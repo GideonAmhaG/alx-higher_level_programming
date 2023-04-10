c339fd33e02693dc167b25fef9a11a702a127503
GideonAmhaG <gideonamha@gmail.com>
2023-04-10 15:43:33 +0300

    1st commit

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const len = process.argv.length;
+if (len <= 3) {
+  console.log('0');
+} else {
+  const args = process.argv.map(Number)
+    .slice(2, len)
+    .sort((a, b) => a - b);
+  console.log(args[args.length - 2]);
+}
