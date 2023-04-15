9f060db0c207ab2bd57b70756fa37bacd4913c6f
GideonAmhaG <gideonamha@gmail.com>
2023-04-15 20:05:50 +0300

    1st commit

---
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""module for a script prints the State object with the name passed as
+argument from the database hbtn_0e_6_usa"""
+from sys import argv
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}".format(
+                            argv[1], argv[2], argv[3]))
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    for instance in session.query(State).filter(State.name.contains("a")):
+        print("{}: {}".format(instance.id, instance.name))
