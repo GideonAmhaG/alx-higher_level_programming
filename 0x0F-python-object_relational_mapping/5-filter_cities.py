1938b18c2f8b9464860cffd8c254783bd6357e50
GideonAmhaG <gideonamha@gmail.com>
2023-04-15 17:31:46 +0300

    1st commit

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""module that takes in arguments and displays all values in the states table
+of hbtn_0e_0_usa where name matches the argument. But this time, one that
+is safe from MySQL injections!"""
+import MySQLdb
+from sys import argv
+
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost", port=3306,
+                         user=argv[1], passwd=argv[2], db=argv[3])
+    cur = db.cursor()
+    cur.execute("SELECT cities.id, cities.name, states.name FROM cities\
+                 LEFT JOIN states ON cities.state_id = states.id\
+                 WHERE states.name = %s\
+                 ORDER BY cities.id ASC", (argv[4],))
+    print(", ".join(row[1] for row in cur.fetchall())
