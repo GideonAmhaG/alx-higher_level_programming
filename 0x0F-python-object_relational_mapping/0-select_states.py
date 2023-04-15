488be7fb980546b6bd7c081bff2d041cf148c42b
GideonAmhaG <gideonamha@gmail.com>
2023-04-15 12:31:42 +0300

    typo

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -9,5 +9,5 @@ if __name__ == "__main__":
                          user=argv[1], passwd=argv[2], db=argv[3])
     cur = db.cursor()
     cur.execute("SELECT * FROM states")
-    for state in c.fetchall():
+    for state in cur.fetchall():
         print(state)
