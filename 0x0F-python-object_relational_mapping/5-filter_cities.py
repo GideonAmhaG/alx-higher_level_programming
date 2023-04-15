4c240a1c775bd4afa045a0a956b3439d8bd8c603
GideonAmhaG <gideonamha@gmail.com>
2023-04-15 17:46:29 +0300

    test

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,20 +1,19 @@
 #!/usr/bin/python3
-"""Lists all cities by state passed by user"""
-
+"""module for a script that takes in the name of a state as an argument
+and lists all cities of that state, using the database"""
 import MySQLdb
 from sys import argv
 
+
 if __name__ == "__main__":
-    conn = MySQLdb.connect(host="localhost", port=3306, charset="utf8",
-                           user=argv[1], passwd=argv[2], db=argv[3])
-    cur = conn.cursor()
-    cur.execute("""
-        SELECT cities.id, cities.name, states.name FROM cities
-        LEFT JOIN states ON cities.state_id = states.id
-        WHERE states.name = %s
-        ORDER BY cities.id ASC;
-        """, (argv[4],))
-    query_rows = cur.fetchall()
-    print(", ".join([row[1] for row in query_rows]))
+    db = MySQLdb.connect(host="localhost", port=3306,
+                         user=argv[1], passwd=argv[2], db=argv[3])
+    cur = db.cursor()
+    cur.execute("""SELECT cities.id, cities.name, states.name FROM cities
+                 LEFT JOIN states ON cities.state_id = states.id
+                 WHERE states.name = %s
+                 ORDER BY cities.id ASC""", (argv[4],))
+    for row in cur.fetchall():
+        print(", ".join(row[1]))
     cur.close()
-    conn.close()
+    db.close()
