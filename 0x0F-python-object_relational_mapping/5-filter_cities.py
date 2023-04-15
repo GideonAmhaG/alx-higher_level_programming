e9cef01f57cb61a0e4c0d7754b07229c0bd0deef
GideonAmhaG <gideonamha@gmail.com>
2023-04-15 17:36:03 +0300

    added more quotes

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -10,8 +10,9 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", port=3306,
                          user=argv[1], passwd=argv[2], db=argv[3])
     cur = db.cursor()
-    cur.execute("SELECT cities.id, cities.name, states.name FROM cities\
-                 LEFT JOIN states ON cities.state_id = states.id\
-                 WHERE states.name = %s\
-                 ORDER BY cities.id ASC", (argv[4],))
-    print(", ".join(row[1] for row in cur.fetchall())
+    cur.execute("""SELECT cities.id, cities.name, states.name FROM cities
+                 LEFT JOIN states ON cities.state_id = states.id
+                 WHERE states.name = %s
+                 ORDER BY cities.id ASC;""", (argv[4],))
+    for row in cur.fetchall():
+        print(", ".join(row[1]))
