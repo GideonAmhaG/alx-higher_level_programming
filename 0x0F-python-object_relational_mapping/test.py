459966500652722f9e7068e18408ea54703569c5
GideonAmhaG <gideonamha@gmail.com>
2023-04-15 12:16:09 +0300

    test

---
+++ 0x0F-python-object_relational_mapping/test.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""module that lists all states from the database hbtn_0e_0_usa"""
+import MySQLdb
+from sys import argv
+
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
+    cur = db.cursor()
+    c.execute("SELECT * FROM states")
+    for state in c.fetchall():
+        print(state)
