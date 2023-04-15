170a8355151f10ebedb80c5cab7b3b0bbe876738
GideonAmhaG <gideonamha@gmail.com>
2023-04-15 17:02:41 +0300

    new code

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -11,7 +11,7 @@ if __name__ == "__main__":
                          user=argv[1], passwd=argv[2], db=argv[3])
     cur = db.cursor()
     cur.execute("SELECT * FROM states\
-                 WHERE name LIKE BINARY %(name)s\
-                 ORDER BY states.id ASC".format(argv[4]))
+                 WHERE name  = %s\
+                 ORDER BY states.id ASC", (argv[4], ))
     for row in cur.fetchall():
         print(row)
