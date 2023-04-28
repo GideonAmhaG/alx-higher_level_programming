70d5c89794141c05c65020927d1e7909713383e7
GideonAmhaG <gideonamha@gmail.com>
2023-04-28 19:55:37 +0300

    typo

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-""" Uses requests module. Prints error code"""
+"""takes in a letter and sends a POST request to
+http://0.0.0.0:5000/search_user with the letter as a parameter"""
 import requests
 from sys import argv
 
