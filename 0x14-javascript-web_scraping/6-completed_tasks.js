5360341ce21fdda18890f984e0dc5c1c5280bf89
GideonAmhaG <gideonamha@gmail.com>
2023-05-23 14:38:30 +0300

    2nd commit

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 const request = require('request');
 request(process.argv[2], (error, response, body) => {
-  !error && console.log(JSON.parse(body).reduce(function (all, curr) {
-    curr.completed && (all[curr.userId] = (all[curr.userId] || 0) + 1);
+  !error && console.log(JSON.parse(body).reduce(function (all, cur) {
+    cur.completed && (all[cur.userId] = (all[cur.userId] || 0) + 1);
     return all;
   }, {}));
 });
